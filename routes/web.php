9ba6b11859283fcad572a30326eecadfe2d3b104
Inácio Barboza <108500066+InacioBrI@users.noreply.github.com>
2025-05-07 20:13:53 -0300

    adicionando login e cadastro

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('About');
+Route::get('/login', function () {
+    return view('Login');
+});
+
+Route::get('/cadastro', function () {
+    return view('Cadastro');
 });
