af0986923003d837f58be5a2bcea94438884b0d1
Inácio Barboza <108500066+InacioBrI@users.noreply.github.com>
2025-07-10 17:26:41 -0300

    excluindo arquivos desnecessario que não fazem juz ao que está o projeto agora

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>FMU - CRUD</title>
+  <title>Dashboard</title>
   <style>
     * {
       margin: 0;
@@ -249,7 +249,7 @@
   @endif
 
   <header>
-    <h1>FMU</h1>
+    <h1>Dashboard</h1>
     <div class="profile-container">
       <div class="profile-icon">👤</div>
         <div class="profile-dropdown">
@@ -263,11 +263,6 @@
       </div>
     </div>
   </header>
-
-  <div class="simulacao-btn">
-    <a href="{{ route('simulador.bemvindo') }}">Faça uma simulação agora!</a>
-  </div>
-
 
   <table>
     <thead>
